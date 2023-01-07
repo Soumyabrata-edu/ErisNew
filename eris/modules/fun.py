a2377a9e02b02381d9dcdd8a2d4f6b1268334a5f
Soumyabrata-edu <115734398+Soumyabrata-edu@users.noreply.github.com>
2023-01-07 17:09:17 +0530

    Add files via upload

---
+++ eris/modules/fun.py
@@ -0,0 +1,162 @@
+import html
+import random
+import time
+import requests
+import re
+import string
+import asyncio
+from io import BytesIO
+import os
+from pathlib import Path
+
+from telegram import Update, ParseMode, Message
+from telegram.ext import run_async, CallbackContext
+
+import eris.modules.fun_strings as fun_strings
+from eris import dispatcher
+from eris.modules.disable import DisableAbleCommandHandler
+from eris.modules.helper_funcs.chat_status import is_user_admin
+from eris.modules.helper_funcs.extraction import extract_user
+
+
+@run_async
+def slap(update: Update, context: CallbackContext):
+    args = context.args
+    message = update.effective_message
+    chat = update.effective_chat
+    reply_text = message.reply_to_message.reply_text if message.reply_to_message else message.reply_text
+    curr_user = html.escape(message.from_user.first_name)
+    user_id = extract_user(message, args)
+
+    if user_id == context.bot.id:
+        temp = random.choice(fun_strings.SLAP_ERIS_TEMPLATES)
+
+        if isinstance(temp, list):
+            if temp[2] == "tmute":
+                if is_user_admin(chat, message.from_user.id):
+                    reply_text(temp[1])
+                    return
+
+                mutetime = int(time.time() + 60)
+                context.bot.restrict_chat_member(
+                    chat.id,
+                    message.from_user.id,
+                    until_date=mutetime,
+                    can_send_messages=False)
+            reply_text(temp[0])
+        else:
+            reply_text(temp)
+        return
+
+    if user_id:
+        slapped_user = context.bot.get_chat(user_id)
+        user1 = curr_user
+        user2 = html.escape(slapped_user.first_name)
+    else:
+        user1 = context.bot.first_name
+        user2 = curr_user
+
+    temp = random.choice(fun_strings.SLAP_TEMPLATES)
+    item = random.choice(fun_strings.ITEMS)
+    hit = random.choice(fun_strings.HIT)
+    throw = random.choice(fun_strings.THROW)
+
+    reply = temp.format(
+        user1=user1,
+        user2=user2,
+        item=item,
+        hits=hit,
+        throws=throw)
+
+    reply_text(reply, parse_mode=ParseMode.HTML)
+
+
+@run_async
+def pat(update: Update, _):
+    msg = update.effective_message
+    pat = requests.get("https://some-random-api.ml/animu/pat").json()
+    link = pat.get("link")
+    if not link:
+        msg.reply_text("No URL was received from the API!")
+        return
+    msg.reply_video(link)
+
+
+@run_async
+def hug(update: Update, _):
+    msg = update.effective_message
+    hug = requests.get("https://some-random-api.ml/animu/hug").json()
+    link = hug.get("link")
+    if not link:
+        msg.reply_text("No URL was received from the API!")
+        return
+    msg.reply_video(link)
+
+
+@run_async
+def insult(update: Update, _):
+    msg = update.effective_message
+    reply_text = msg.reply_to_message.reply_text if msg.reply_to_message else msg.reply_text
+    reply_text(random.choice(fun_strings.INSULT_STRINGS))
+
+
+@run_async
+def shrug(update: Update, _):
+    msg = update.effective_message
+    reply_text = msg.reply_to_message.reply_text if msg.reply_to_message else msg.reply_text
+    reply_text(r"¯\_(ツ)_/¯")
+
+
+@run_async
+def table(update: Update, _):
+    reply_text = update.effective_message.reply_to_message.reply_text if update.effective_message.reply_to_message else update.effective_message.reply_text
+    reply_text(random.choice(fun_strings.TABLE))
+
+
+__help__ = """
+-> `/slap`
+slap a user, or get slapped if not a reply.
+-> `/shrug`
+get shrug XD.
+-> `/table`
+get flip/unflip :v.
+-> `/insult`
+Insults the retar
+-> `/pat`
+pats a user by a reply to the message
+-> `/hug`
+hugs a user by a reply to the message
+"""
+
+
+PAT_HANDLER = DisableAbleCommandHandler("pat", pat)
+HUG_HANDLER = DisableAbleCommandHandler("hug", hug)
+SLAP_HANDLER = DisableAbleCommandHandler("slap", slap, pass_args=True)
+SHRUG_HANDLER = DisableAbleCommandHandler("shrug", shrug)
+TABLE_HANDLER = DisableAbleCommandHandler("table", table)
+INSULT_HANDLER = DisableAbleCommandHandler("insult", insult)
+
+dispatcher.add_handler(SLAP_HANDLER)
+dispatcher.add_handler(SHRUG_HANDLER)
+dispatcher.add_handler(TABLE_HANDLER)
+dispatcher.add_handler(INSULT_HANDLER)
+dispatcher.add_handler(PAT_HANDLER)
+dispatcher.add_handler(HUG_HANDLER)
+
+__mod_name__ = "Fun"
+
+__command_list__ = [
+    "slap",
+    "shrug",
+    "table",
+    "insult",
+    "pat",
+    "hug"]
+
+__handlers__ = [
+    SLAP_HANDLER,
+    SHRUG_HANDLER,
+    TABLE_HANDLER,
+    INSULT_HANDLER,
+    PAT_HANDLER,
+    HUG_HANDLER]
---
+++ eris/modules/special.py
@@ -0,0 +1,260 @@
+import asyncio
+
+from telethon import events
+from telethon.errors import UserNotParticipantError
+from telethon.tl.functions.channels import GetParticipantRequest
+from telethon.tl.types import ChannelParticipantAdmin
+from telethon.tl.types import ChannelParticipantCreator
+
+from eris import TEMP_DOWNLOAD_DIRECTORY, telethn as client
+
+spam_chats = []
+
+
+@client.on(events.NewMessage(pattern="^/tagall|@all|/all ?(.*)"))
+async def mentionall(event):
+    chat_id = event.chat_id
+    if event.is_private:
+        return await event.respond("__This command can be use in groups and channels!__")
+
+    is_admin = False
+    try:
+        partici_ = await client(GetParticipantRequest(
+            event.chat_id,
+            event.sender_id
+        ))
+    except UserNotParticipantError:
+        is_admin = False
+    else:
+        if (
+                isinstance(
+                    partici_.participant,
+                    (
+                            ChannelParticipantAdmin,
+                            ChannelParticipantCreator
+                    )
+                )
+        ):
+            is_admin = True
+    if not is_admin:
+        return await event.reply("__Only admins can mention all!__")
+
+    if event.pattern_match.group(1) and event.is_reply:
+        return await event.reply("__Give me one argument!__")
+    elif event.pattern_match.group(1):
+        mode = "text_on_cmd"
+        msg = event.pattern_match.group(1)
+    elif event.is_reply:
+        mode = "text_on_reply"
+        msg = await event.get_reply_message()
+        if msg == None:
+            return await event.respond(
+                "__I can't mention members for older messages! (messages which are sent before I'm added to group)__")
+    else:
+        return await event.reply("__Reply to a message or give me some text to mention others!__")
+
+    spam_chats.append(chat_id)
+    usrnum = 0
+    usrtxt = ''
+    async for usr in client.iter_participants(chat_id):
+        if not chat_id in spam_chats:
+            break
+        usrnum += 1
+        usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}), "
+        if usrnum == 5:
+            if mode == "text_on_cmd":
+                txt = f"{msg}\n{usrtxt}"
+                await client.send_message(chat_id, txt)
+            elif mode == "text_on_reply":
+                await msg.reply(usrtxt)
+            await asyncio.sleep(2)
+            usrnum = 0
+            usrtxt = ''
+    try:
+        spam_chats.remove(chat_id)
+    except:
+        pass
+
+
+@client.on(events.NewMessage(pattern="^/cancel$"))
+async def cancel_spam(event):
+    is_admin = False
+    try:
+        partici_ = await client(GetParticipantRequest(
+            event.chat_id,
+            event.sender_id
+        ))
+    except UserNotParticipantError:
+        is_admin = False
+    else:
+        if (
+                isinstance(
+                    partici_.participant,
+                    (
+                            ChannelParticipantAdmin,
+                            ChannelParticipantCreator
+                    )
+                )
+        ):
+            is_admin = True
+    if not is_admin:
+        return await event.reply("__Only admins can execute this command!__")
+    if not event.chat_id in spam_chats:
+        return await event.reply("__There is no proccess on going...__")
+    else:
+        try:
+            spam_chats.remove(event.chat_id)
+        except:
+            pass
+        return await event.respond("__Stopped Mention.__")
+
+import os
+from datetime import datetime
+
+from PIL import Image
+from telegraph import Telegraph, exceptions, upload_file
+
+from eris.events import register
+
+telegraph = Telegraph()
+r = telegraph.create_account(short_name="eris")
+auth_url = r["auth_url"]
+
+
+@register(pattern="^/tg(m|t) ?(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    optional_title = event.pattern_match.group(2)
+    if event.reply_to_msg_id:
+        start = datetime.now()
+        r_message = await event.get_reply_message()
+        input_str = event.pattern_match.group(1)
+        if input_str == "m":
+            downloaded_file_name = await client.download_media(
+                r_message, TEMP_DOWNLOAD_DIRECTORY
+            )
+            end = datetime.now()
+            ms = (end - start).seconds
+            h = await event.reply(
+                "Downloaded to {} in {} seconds.".format(downloaded_file_name, ms)
+            )
+            if downloaded_file_name.endswith((".webp")):
+                resize_image(downloaded_file_name)
+            try:
+                start = datetime.now()
+                media_urls = upload_file(downloaded_file_name)
+            except exceptions.TelegraphException as exc:
+                await h.edit("ERROR: " + str(exc))
+                os.remove(downloaded_file_name)
+            else:
+                end = datetime.now()
+                (end - start).seconds
+                os.remove(downloaded_file_name)
+                await h.edit(
+                    "Uploaded to https://te.legra.ph{})".format(media_urls[0]),
+                    link_preview=True,
+                )
+        elif input_str == "t":
+            user_object = await client.get_entity(r_message.sender_id)
+            title_of_page = user_object.first_name  # + " " + user_object.last_name
+            if optional_title:
+                title_of_page = optional_title
+            page_content = r_message.message
+            if r_message.media:
+                if page_content != "":
+                    title_of_page = page_content
+                downloaded_file_name = await client.download_media(
+                    r_message, TEMP_DOWNLOAD_DIRECTORY
+                )
+                m_list = None
+                with open(downloaded_file_name, "rb") as fd:
+                    m_list = fd.readlines()
+                for m in m_list:
+                    page_content += m.decode("UTF-8") + "\n"
+                os.remove(downloaded_file_name)
+            page_content = page_content.replace("\n", "<br>")
+            response = telegraph.create_page(title_of_page, html_content=page_content)
+            end = datetime.now()
+            ms = (end - start).seconds
+            await event.reply(
+                "Pasted to https://telegra.ph/{} in {} seconds.".format(
+                    response["path"], ms
+                ),
+                link_preview=True,
+            )
+    else:
+        await event.reply("Reply to a message to get a permanent telegra.ph link.")
+
+
+def resize_image(image):
+    im = Image.open(image)
+    im.save(image, "PNG")
+
+import requests
+from eris import dispatcher
+from eris.modules.disable import DisableAbleCommandHandler
+from telegram import Update, ParseMode
+from telegram.ext import CallbackContext, run_async
+
+def github(update: Update, context: CallbackContext):
+    bot = context.bot
+    message = update.effective_message
+    args = message.text.split(" ", 1)
+    
+    if len(args) == 1:
+        message.reply_text('Provide me Username, Ex - /git Noob-Kittu')
+        return
+    else:
+        pass
+    username = args[1]
+    URL = f'https://api.github.com/users/{username}'
+    with requests.get(URL) as request:
+            if request.status_code == 404:
+                return message.reply_text("404")
+
+            result = request.json()
+            try:
+                url = result['html_url']
+                name = result['name']
+                company = result['company']
+                bio = result['bio']
+                created_at = result['created_at']
+                avatar_url = result['avatar_url']
+                blog = result['blog']
+                location = result['location']
+                repositories = result['public_repos']
+                followers = result['followers']
+                following = result['following']
+                caption = f"""**Info Of {name}**
+**Username:** `{username}`
+**Bio:** `{bio}`
+**Profile Link:** [Here]({url})
+**Company:** `{company}`
+**Created On:** `{created_at}`
+**Repositories:** `{repositories}`
+**Blog:** `{blog}`
+**Location:** `{location}`
+**Followers:** `{followers}`
+**Following:** `{following}`"""
+            except Exception as e:
+                print(str(e))
+                pass
+    message.reply_photo(photo=avatar_url, caption=caption, parse_mode=ParseMode.MARKDOWN)
+
+GIT_HANDLER = DisableAbleCommandHandler("github", github, run_async=True)
+dispatcher.add_handler(GIT_HANDLER)
+
+__mod_name__ = "Special"
+
+__help__ = """
+──「 *Owner only:* 」──
+-> /tagall
+Tag all members from a chat
+
+-> /tgm or /tgt 
+Convert to telegra.ph
+
+--> /github <username>
+Send your github profile in dm.
+"""
