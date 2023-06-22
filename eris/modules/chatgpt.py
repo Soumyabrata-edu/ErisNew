7529a2c2f162215932ce4de78f7da8709b8443a6
Soumyabrata-edu <115734398+Soumyabrata-edu@users.noreply.github.com>
2023-06-22 21:54:43 +0530

    Update chatgpt.py

--- eris/modules/chatgpt.py
+++ eris/modules/chatgpt.py
@@ -1,19 +1,21 @@
 import requests
 from eris import pbot
 from pyrogram import Client, filters
+from telegram import Update, Bot
 
-@pbot.on_message(filters.command('chat'))
-def handle_chat(message):
+@pbot.on_message(filters.command('ask'))
+def handle_chat(update: Update, bot: Bot):
 
-  command = '/chat'
-  text = message.text.replace(command, '', 1).strip()
-
+  utext = update.message.text
+  text = utext.text.replace('/ask', '', 1).strip() 
+  
   if text:
+    
     url = 'https://amirroboti.eliyahost.ir/ApiWeb/ChatGPT1.php?text=' + text
     response = requests.get(url)
     ResponseText = response.text
 
-    bot.send_message(ResponseText)
+    bot.send_message(chat_id=update.message.chat_id, text= ResponseText)
 
   else:
     bot.send_message("Report errors at @bloggerminds")
